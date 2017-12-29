10945daecb954d12017d7b417ac11c1b228e8fb6
Oliver Wolf <root@streacs.com>
2017-12-30 00:39:45 +0100

    added more directory tests

--- rspec-specs/spec/localhost/server_spec.rb
+++ rspec-specs/spec/localhost/server_spec.rb
@@ -6,3 +6,15 @@ describe file('/home/crowd') do
   it { should be_owned_by 'crowd' }
   it { should be_grouped_into 'crowd' }
 end
+
+describe file('/opt/atlassian/crowd') do
+  it { should be_directory }
+  it { should be_owned_by 'crowd' }
+  it { should be_grouped_into 'crowd' }
+end
+
+describe file('/var/opt/atlassian/application-data/crowd') do
+  it { should be_directory }
+  it { should be_owned_by 'crowd' }
+  it { should be_grouped_into 'crowd' }
+end
