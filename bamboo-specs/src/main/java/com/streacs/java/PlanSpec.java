b387dada55a680f9796e94721bb677c4f2096bd0
Oliver Wolf <root@streacs.com>
2017-12-31 13:14:43 +0100

    removed old script task

--- bamboo-specs/src/main/java/com/streacs/java/PlanSpec.java
+++ bamboo-specs/src/main/java/com/streacs/java/PlanSpec.java
@@ -88,9 +88,6 @@ public class PlanSpec {
                     new Job("Default Job", "F33E1A")
                         .requirements(new Requirement("system.docker.executable"))
                         .tasks(
-                            scriptTask()
-                        )
-                        .tasks(
                             checkoutTask()
                         )
                         .tasks(
@@ -98,12 +95,6 @@ public class PlanSpec {
                         )
                 )
             );
-    }
-
-    ScriptTask scriptTask() {
-        return new ScriptTask()
-            .inlineBody("echo 'hello world'")
-            .interpreterShell();
     }
 
     VcsCheckoutTask checkoutTask() {
